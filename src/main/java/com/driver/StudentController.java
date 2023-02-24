73abbace55879864c7bb32ad22ae2661e0701849
SUMAN SAURABH <110984246+Git-suman309@users.noreply.github.com>
2023-02-24 17:42:48 +0530

    error in line 45 Student student =null ;

--- src/main/java/com/driver/StudentController.java
+++ src/main/java/com/driver/StudentController.java
@@ -42,29 +42,28 @@ public class StudentController {
 
     @GetMapping("/get-student-by-name/{name}")
     public ResponseEntity<Student> getStudentByName(@PathVariable String name){
-        Student student = null; // Assign student by calling service layer method
-        s.getStudentByName(name);
+        Student student = s.getStudentByName(name); // Assign student by calling service layer method
         return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-teacher-by-name/{name}")
     public ResponseEntity<Teacher> getTeacherByName(@PathVariable String name){
-        Teacher teacher = null; // Assign student by calling service layer method
-        s.getTeacherByName(name);
+        Teacher teacher =  s.getTeacherByName(name); // Assign student by calling service layer method
+
         return new ResponseEntity<>(teacher, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-students-by-teacher-name/{teacher}")
     public ResponseEntity<List<String>> getStudentsByTeacherName(@PathVariable String teacher){
-        List<String> students = null; // Assign list of student by calling service layer method
-        s.getStudentsByTeacherName(teacher);
+        List<String> students =  s.getStudentsByTeacherName(teacher); // Assign list of student by calling service layer method
+
         return new ResponseEntity<>(students, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-all-students")
     public ResponseEntity<List<String>> getAllStudents(){
-        List<String> students = null; // Assign list of student by calling service layer method
-        s.getAllStudents();
+        List<String> students = s.getAllStudents();// Assign list of student by calling service layer method
+
         return new ResponseEntity<>(students, HttpStatus.CREATED);
     }
 
